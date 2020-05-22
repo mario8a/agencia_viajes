7e425bfbcb11c5dd0978ce5935931c3bd5f58071
mario8a <mario8ato@hotmail.com>
2020-05-22 18:06:41 -0500

    Proyecto terminado

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 //importar express
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const configs = require('./config');
 
-const db = require('./config/database');
 
-db.authenticate()
-    .then(() => console.log('DB conectada'))
-    .catch(error => console.log(error))
+
+// db.authenticate()
+//     .then(() => console.log('DB conectada'))
+//     .catch(error => console.log(error))
 
 //configurar express
 const app = express();
@@ -28,14 +29,18 @@ const config = configs[app.get('env')];
 //creamos la variable para el sitio web
 app.locals.titulo = config.nombresitio
 
-//Muestra el año actual
+//Muestra el año actual y genera la ruta
 app.use((req,res,next) => {
     //creamos una nueva fecha
     const fecha = new Date();
     res.locals.fechaActual = fecha.getFullYear();
+    res.locals.ruta = req.path;
     // console.log(res.locals);
     return next();
 })
+
+//ejecutamos el body parser
+app.use(bodyParser.urlencoded({extended: true}))
 
 //cargar las rutas
 app.use('/', routes())
